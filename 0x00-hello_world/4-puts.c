ec77848c61a8232cb3c108dc8b4b3b5ea41bb90e
didigwua <didigwuabuchi@gmail.com>
2022-06-30 18:45:14 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,17 @@
 /
 
  * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
- *   */
 
- int main(void)
+ * Return: Always 0 (sucess)
+
+ */
+
+int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle);
 
-	return (0);
+return (0);
 
 }
