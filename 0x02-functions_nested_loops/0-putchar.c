6ab517280e9addbc3613612b174443e516954761
didigwua <didigwuabuchi@gmail.com>
2022-07-05 16:46:29 -0700

    Task try again commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints putchar
- *
- * Return: Always 0
+ * Return: always 0
  */
 int main(void)
 {
-	_putchar('P');
+	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
