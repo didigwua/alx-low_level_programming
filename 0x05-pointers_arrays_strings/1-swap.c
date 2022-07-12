23621a096fbe30aa6463bf5b42d43c9d05771cc6
didigwua <didigwuabuchi@gmail.com>
2022-07-12 05:32:51 -0700

    Pointer task 1 commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,18 @@
-#include "main.h"
-#include <stdio.h>
+include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+  * swap_int - Swaps the values of two integers
+  *
+  * @a: An integer to swap
+  * @b: Another integer to swap
+  *
+  * Return: void
+  */
+void swap_int(int *a, int *b)
 {
-    int a;
-    int b;
+	int aux;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+	aux = *a;
+	*a = *b;
+	*b = aux;
 }
