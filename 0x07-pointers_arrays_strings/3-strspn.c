cf7d284602dcae19d3f269ea5cb9db7d543a6bb0
didigwua <didigwuabuchi@gmail.com>
2022-07-18 06:35:25 -0700

    task 3-strspn.c commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - search a string for a set of bytes
+ * @s: source string
+ * @accept: accespted string
+ *
+ * Return: number of bytes in the init segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unasugned int a = 0, b, t = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+		while (s[b] != 32)
+		{
+			if (accepted[a] == s[b])
+			{
+				t++;
+			}
+
+			b++;
+		}
+
+		return (t);
+	}
