2c9001a2123140e013baba7a4abb96367b1ece1a
didigwua <didigwuabuchi@gmail.com>
2022-07-18 05:28:57 -0700

    Task 1-memcpy.c commit redo

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,18 +3,18 @@
 /**
  * _memcpy - copy memory area
  * @dest: dest memory area
- * @src: dource memory area
- * @n: bytes fron memory area
+ * @src: source memory area
+ * @n: bytes from memory area to copy
  *
- * Return memory area replaced
+ * Return: memory area replaced
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int a;
+	unasigned int a;
 
-	for  (a = 0; a < n; a++)
+	for (a = 0; a < n; a++)
 	{
-		dest[a] = src[a];
+		dest[a] = src [a];
 	}
 
 	return (dest);
